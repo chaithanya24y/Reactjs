0ef1bbcd8ecd4118511c1c174724c04ce648a6f8
chaitanya24y <chaitanyameraga@gmail.com>
2022-08-04 12:07:21 +0530

    added app.js ,package.json and gitignore files

---
+++ app.js
@@ -0,0 +1,8 @@
+let express = require('express');
+let record=express();
+ record.listen(2500,function(){
+   // console.log(2500,'pharmasutical company');
+ });
+ record.get('/getUser',function(req,res){
+     res.send("your data sucessfully on our company");
+ });
