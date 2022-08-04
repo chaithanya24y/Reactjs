8af97eb6df06e9b3a71a5d61df4c4256a03d12df
chaitanya24y <chaitanyameraga@gmail.com>
2022-08-04 20:17:11 +0530

    added post request

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 let express = require('express');
 let record=express();
  record.listen(2500,function(){
-   // console.log(2500,'pharmasutical company');
+   console.log(2500,'pharmasutical company');
  });
+
+ record.use(express.json());
  record.get('/getUser',function(req,res){
      res.send("your data sucessfully on our company");
  });
  record.get('/getemploye',function(req,res){
     res.send('sucess');
  });
+
+ record.post('/createUser',function(req,res){
+  res.send("chaitanya");
+});
